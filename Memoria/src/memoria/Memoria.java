5c37abbb5467e0baefc2c4a5d9d04ac389d1e1ad
Samueldu9 <61156568+Samueldu9@users.noreply.github.com>
2020-03-21 23:43:00 -0500

    Add files via upload

---
+++ Memoria/src/memoria/Memoria.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+/**
+ *
+ * @author FeGa
+ */
+public class Memoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmMemoria frmmemoria = new FrmMemoria();
+        frmmemoria.setVisible(true);
+    }
+    
+}
